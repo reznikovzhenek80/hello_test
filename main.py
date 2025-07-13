f94ff0bcee81dbd336a56fab45211c7230dc95b3
lakustovdimka <lakustovdimka@gmail.com>
2025-07-13 23:20:28 +0500

    12

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_weather(latitude, longitude):
     except requests.exceptions.RequestException as e:
         print(f"Ошибкdfdfа при запросе gghgпогоды: {e}")
     except KeyError as e:
-        print(f"Ошибка при пasdaарdaasdfsdfsfdsssdadadвzxыввыууунге данных: Отсутствует ключ {e}")
+        print(f"Ошибка при пasdaарddfsdfsfdsssdadadвzxыввыууунге данных: Отсутствует ключ {e}")
     except Exception as e:
         print(f"Произошла непредвиденная ошибка: {e}")
 
